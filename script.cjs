3902c9cc0be614af2864aefaa9cf973b7c00aaba
legendSabbir <mmax206602@gmail.com>
2024-06-25 06:00:57 +0600

    workflow

---
+++ script.cjs
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+const data = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+
+const brooch = data.filter(p => p.category === "brooch");
+const nonBrooch = data.filter(p => p.category !== "brooch");
+const updatedData = [...nonBrooch, ...brooch].filter(p => p.stock > 0).map(p => {
+  delete p.stock
+  return p
+});
+
+fs.writeFileSync("data.json", JSON.stringify(updatedData));
