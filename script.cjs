1f1eea8d0c3d84e7a001c37bb2daf705b4798185
legendSabbir <mmax206602@gmail.com>
2024-06-25 06:10:33 +0600

    workflow

--- script.cjs
+++ script.cjs
@@ -6,6 +6,7 @@ const brooch = data.filter(p => p.category === "brooch");
 const nonBrooch = data.filter(p => p.category !== "brooch");
 const updatedData = [...nonBrooch, ...brooch].filter(p => p.stock > 0).map(p => {
   delete p.stock
+  delete p._id
   return p
 });
 
