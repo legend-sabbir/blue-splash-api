181e4b4cfcae2022945328bee9de2e94791c4753
legendSabbir <mmax206602@gmail.com>
2024-07-08 12:43:32 +0000

    workflow update

--- script.cjs
+++ script.cjs
@@ -5,11 +5,18 @@ const customized = JSON.parse(fs.readFileSync('customized.json', 'utf8'));
 
 const customizedMap = new Map(customized.data.map(s => [s.productId, s]));
 
-const updatedData = products.map(p => {
-  const item = customizedMap.get(p.productId);
-  const obj = Object.assign(p, item);
-  delete obj._id
-  return obj
-}).sort((a, b) => a.order - b.order);
+const updatedData = products
+  .map(p => {
+    const item = customizedMap.get(p.productId);
+    return Object.assign(p, item);
+  })
+  .sort((a, b) => a.order - b.order)
+  .map(p => {
+    delete p._id
+    delete p.stock
+    delete p.order
+    return p
+  })
+  .filter(p => p.stock > 0);
 
 fs.writeFileSync("data.json", JSON.stringify(updatedData));
