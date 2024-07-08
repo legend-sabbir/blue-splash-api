438720ef8d589fb4bd1cb8e83d60e76dc71bdfaf
legendSabbir <mmax206602@gmail.com>
2024-07-08 12:31:00 +0000

    workflow update

--- script.cjs
+++ script.cjs
@@ -1,13 +1,15 @@
 const fs = require('fs');
 
-const data = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+const products = JSON.parse(fs.readFileSync('products.json', 'utf8'));
+const customized = JSON.parse(fs.readFileSync('customized.json', 'utf8'));
 
-const brooch = data.filter(p => p.category === "brooch");
-const nonBrooch = data.filter(p => p.category !== "brooch");
-const updatedData = [...nonBrooch, ...brooch].filter(p => p.stock > 0).map(p => {
-  delete p.stock
-  delete p._id
-  return p
-});
+const customizedMap = new Map(customized.data.map(s => [s.productId, s]));
+
+const updatedData = products.map(p => {
+  const item = customizedMap.get(p.productId);
+  const obj = Object.assign(p, item);
+  delete obj._id
+  return obj
+}).sort((a, b) => a.order - b.order);
 
 fs.writeFileSync("data.json", JSON.stringify(updatedData));
